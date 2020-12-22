47c41d853ec6c7fc9f55e5a5ec2a9d8ab4a1275b
D.Mohan reddy <68439993+555-mohanreddy@users.noreply.github.com>
2020-12-22 12:52:31 +0530

    Update constants.php

--- task_master/config/constants.php
+++ task_master/config/constants.php
@@ -4,10 +4,9 @@ session_start();
 
 //Create Constants to save Database Credentials
 define('LOCALHOST', 'localhost');
-define('DB_USERNAME', 'mohansql3'); //Your Database username instead of 'root'
-define('DB_PASSWORD', 'mohanreddy123'); //Your Database Password instead of null/empty
-define('DB_NAME', 'task_manager'); //Your Database Name if it's not 'task_manager'
+define('DB_USERNAME', ''); // Database username 
+define('DB_PASSWORD', ''); // Database Password 
+define('DB_NAME', 'task_manager'); // Database Name 
 
-define('SITEURL', 'http://localhost/task-manager/'); //Update the home URL of the project if you have changed port number or it's live on server
-
+define('SITEURL', 'http://localhost/task-manager/'); 
 ?>
